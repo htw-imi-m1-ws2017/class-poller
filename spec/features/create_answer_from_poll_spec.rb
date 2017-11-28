1f067c6a2f203f9bb56ae36d156d37268dcba024
B Kleinen <kleinen@infrastructure.de>
2017-11-28 23:14:03 +0100

    actual test for association

--- spec/features/create_answer_from_poll_spec.rb
+++ spec/features/create_answer_from_poll_spec.rb
@@ -9,7 +9,12 @@ describe Poll do
   end
   it 'creates answer with the correct poll id' do
     visit poll_path(@poll)
+    # note that i've introduced an id here to make the test less brittle
     click_on 'add-answer-link'
     expect(page).to have_content "New Answer for #{@poll.question}"
+    fill_in 'Text', with: 'Rails'
+    # https://relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/change-matcher
+    expect { click_on 'Create Answer' }.to change { @poll.answers.count }.by(1)
+    expect(page).to have_content('Rails')
   end
 end
